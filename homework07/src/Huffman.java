838f7f9034de6c1a71abe9114e205e3ee4bd1407
iCAROS7 <hominlab@gmail.com>
2021-11-12 14:32:13 +0900

    허프만 코드 출력 메서드 추가

--- homework07/src/Huffman.java
+++ homework07/src/Huffman.java
@@ -45,6 +45,13 @@ public class Huffman {
         }
     }
 
+    // 허프만 코드 배열 출력 메서드
+    private void printArr(int[] arr, int cur) {
+        for(int i = 0; i < cur; i++) { System.out.print(arr[i]); }
+        System.out.print("  ");
+    }
+
+    // 삽입 메서드
     public void insert(int freq, String val, Entry l, Entry r, String s) {
         Entry temp = new Entry(freq, val, l, r, s); // Entry 생성
         a[++N] = temp;                              // 새로운 키를 배열 마지막 항목 다음에 저장
@@ -62,5 +69,39 @@ public class Huffman {
         downHeap(1);
 
         return min;
+    }
+
+    // 트리 생성 메서드
+    public Entry createTree() {
+        while (getSize() > 1) {     // 힙에 1개의 노드만 남을 떄 까지
+            Entry e1 = deleteMin(); // 힙에서 최소 빈도 수 가진 노드를 제거하여 e1이 참조
+            Entry e2 = deleteMin(); // 힙에서 최소 빈도 수 가진 노드를 제거하여 e2가 참조
+            Entry temp = new Entry(e1.getKey() + e2.getKey(), e1.getValue() + e2.getKey(), e1, e2, " ");
+            insert(temp);
+        }
+
+        return deleteMin(); // 루트노드를 힙에서 제거하며 리턴
+    }
+
+    // 전위 순회하며 허프만 코드 출력 메서드
+    public void preOrder(Entry entry, int[] arr, int cur) {
+        if (entry.getLeft() != null) {
+            arr[cur] = 0; // left 이동시 배열에 0 추가
+            preOrder(entry.getLeft(), arr, cur + 1);
+        }
+        if (entry.getRight() != null) {
+            arr[cur] = 1; // right 이동시 배열에 1 추가
+            preOrder(entry.getRight(), arr, cur + 1);
+        }
+        if (entry.getRight() == null && entry.getLeft() == null) {
+            System.out.print(entry.getValue() + ": ");
+            printArr(arr, cur);
+        }
+    }
+
+    // Entry 배열 출력 메서드
+    public void print() {
+        for (int i = 1; i < N; i++) { System.out.print("[" + a[i].getKey() + " " + a[i].getValue() + "] "); }
+        System.out.println();
     }
 }
