90b4c1f015cbfacdaee113bdc29758573d8e4ab2
iCAROS7 <hominlab@gmail.com>
2021-11-12 14:31:47 +0900

    주석 추가

--- homework07/src/Huffman.java
+++ homework07/src/Huffman.java
@@ -8,19 +8,23 @@ public class Huffman {
     private Entry[] a;      // a[0]은 사용 안함
     private int N;          // 힙의 크기
 
+    // 생성자
     public Huffman(Entry[] harray, int initialSize) {
         a = harray;
         N = initialSize;
     }
 
+    // 빈도수 비교 메서드
     private boolean greater(int i, int j) { return a[i].getKey() > a[j].getKey(); }
 
+    // 두 Entry 스왑 메서드
     private void swap(int i, int j) {
         Entry tmp = a[i];
         a[i] = a[j];
         a[j] = tmp;
     }
 
+    // 부모 자식 교환 메서드
     private void downHeap(int i) {      // i는 현재 노드의 인덱스
         while (2 * i <= N) {            // i의 왼쪽 자식노드가 힙에 있으면
             int k = 2 * i;              // k는 왼쪽 자식노드의 인덱스
@@ -33,6 +37,7 @@ public class Huffman {
         }
     }
 
+    // 자식 부모 교환 메서드
     private void upHeap(int j) {
         while (j > 1 && greater(j/2, j)) {
             swap(j/2, j);
@@ -46,8 +51,10 @@ public class Huffman {
         upHeap(N);                                  // 위로 올라가며 힙속성 회복 준비
     }
 
+    // 힙 생성 메서드
     public void createHeap() { for(int i = N/2; i > 0; i--) { downHeap(i); } }
 
+    // 최소값 제거 메서드
     public Entry deleteMin() {
         Entry min = a[1];
         swap(1, N--);
