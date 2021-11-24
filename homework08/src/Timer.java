f95746965f962e2cdf9d1c24dbe259706900b488
iCAROS7 <hominlab@gmail.com>
2021-11-24 18:55:11 +0900

    Timer.java 주석 추가

--- homework08/src/Timer.java
+++ homework08/src/Timer.java
@@ -5,12 +5,12 @@
  */
 
 public class Timer {
-    private static long start, end;
-    private static long calcTimer() { return end - start; }
-    private static void endTimer() { end = System.currentTimeMillis(); }
+    private static long start, end; // 현재 시간 기록용 long 변수
+    private static long calcTimer() { return end - start; } // 소요 시간 계산 메서드
+    private static void endTimer() { end = System.currentTimeMillis(); } // 종료 시간 기록 메서드
 
-    public static void startTimer() { start = System.currentTimeMillis(); }
-    public static void printTimer(String s) {
+    public static void startTimer() { start = System.currentTimeMillis(); } // 시작 시간 기록 메서드
+    public static void printTimer(String s) { // 소요 시간 출력 메서드
         endTimer();
         System.out.println("\n" + s + " 정렬의 결과: 소요 시간 " + calcTimer() + "ms");
     }
